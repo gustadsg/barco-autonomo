308ad382022695c5bedb01e503dacb2904d8a36a
Gustavo Gomes <gustadsg@outlook.com.br>
2023-11-30 19:15:50 -0300

    feat: codigo servo motor

---
+++ Core/Inc/SERVO.h
@@ -0,0 +1,51 @@
+/*
+ * Servo.h
+ *
+ *  Created on: Oct 7, 2023
+ *      Author: gusta
+ */
+
+#ifndef INC_SERVO_H_
+#define INC_SERVO_H_
+
+#include "PWM.h"
+
+#define SERVO_MIN_ANGLE -90;
+#define SERVO_MAX_ANGLE 90;
+#define SERVO_MIDDLE_ANGLE 0;
+#define SERVO_MIN_PERCENT 0;
+#define SERVO_MAX_PERCENT 100;
+
+typedef struct {
+	float min;
+	float max;
+} SERVO_Scale_t;
+
+typedef struct {
+	float offset;
+	float gain;
+} SERVO_Calibration_t;
+
+typedef struct {
+	TIM_HandleTypeDef handle;
+	uint32_t channel;
+	uint32_t period;
+
+	//	porcentagens de duty cycle para ângulos mínimo e máximo
+	float minDutyCyclePercentage;
+	float maxDutyCyclePercentage;
+} SERVO_TimerConfig_t;
+
+typedef struct {
+	SERVO_TimerConfig_t timerConfig;
+	SERVO_Calibration_t calibration;
+} SERVO_Config_t;
+
+void SERVO_SetAngle(SERVO_Config_t servoConfig, float angle);
+void SERVO_SetPercentage(SERVO_Config_t servoConfig, float angle);
+
+float __SERVO_ConvertScales(SERVO_Scale_t from, SERVO_Scale_t to, float point);
+float __SERVO_GetCalibratedAngle(SERVO_Calibration_t calibration, float desiredAngle);
+SERVO_Scale_t __SERVO_GetPWMScale(SERVO_TimerConfig_t timerConfig);
+
+#endif /* INC_SERVO_H_ */
---
+++ Core/Src/SERVO.c
@@ -0,0 +1,64 @@
+/*
+ * Servo.c
+ *
+ *  Created on: Oct 7, 2023
+ *      Author: gusta
+ */
+
+#include "SERVO.h"
+
+void SERVO_SetAngle(SERVO_Config_t servoConfig, float angle) {
+	SERVO_Scale_t angleScale;
+	angleScale.min = SERVO_MIN_ANGLE
+	;
+	angleScale.max = SERVO_MAX_ANGLE
+	;
+
+	SERVO_Scale_t pwmScale = __SERVO_GetPWMScale(servoConfig.timerConfig);
+
+	float convertedToPWM = __SERVO_ConvertScales(angleScale, pwmScale,
+			__SERVO_GetCalibratedAngle(servoConfig.calibration, angle));
+	PWM_SetValue(servoConfig.timerConfig.handle, servoConfig.timerConfig.channel,
+			servoConfig.timerConfig.period, convertedToPWM);
+}
+
+void SERVO_SetPercentage(SERVO_Config_t servoConfig, float angle) {
+	SERVO_Scale_t angleScale;
+	angleScale.min = SERVO_MIN_PERCENT;
+	angleScale.max = SERVO_MAX_PERCENT;
+
+	SERVO_Scale_t pwmScale = __SERVO_GetPWMScale(servoConfig.timerConfig);
+
+	float convertedToPWM = __SERVO_ConvertScales(angleScale, pwmScale,
+			__SERVO_GetCalibratedAngle(servoConfig.calibration, angle));
+	PWM_SetValue(servoConfig.timerConfig.handle, servoConfig.timerConfig.channel,
+			servoConfig.timerConfig.period, convertedToPWM);
+}
+
+SERVO_Scale_t __SERVO_GetPWMScale(SERVO_TimerConfig_t timerConfig) {
+	SERVO_Scale_t pwmScale;
+	pwmScale.min = timerConfig.minDutyCyclePercentage * timerConfig.period;
+	pwmScale.max = timerConfig.maxDutyCyclePercentage * timerConfig.period;
+
+	return pwmScale;
+}
+
+float __SERVO_ConvertScales(SERVO_Scale_t from, SERVO_Scale_t to, float point) {
+	float deltaScaleFrom = from.max - from.min;
+	float deltaScaleTo = to.max - to.min;
+
+	float result = ((point - from.min) / deltaScaleFrom) * deltaScaleTo
+			+ to.min;
+
+	if(result > to.max) return to.max;
+	if(result < to.min) return to.min;
+
+	return result;
+}
+
+/*
+ * Foi observado que há imprecisões no servo motor, de forma a ser necessário ajustar o valor enviado conforme a reta de calibração obtida
+ */
+float __SERVO_GetCalibratedAngle(SERVO_Calibration_t calibration, float desiredAngle) {
+	return calibration.gain * desiredAngle + calibration.offset; // y = a*x + b
+}
